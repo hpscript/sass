42bc4bacb2667b3dd148023ac2791d4d7d097b2a
hpscript <43508837+hpscript@users.noreply.github.com>
2019-11-22 11:54:51 +0900

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,33 @@
 var gulp = require('gulp');
+var imagemin = require('gulp-imagemin');
+var webserver = require('gulp-webserver');
 
 gulp.task('html', function(done){
-	gulp.src('./src/index.html')
-		.pipe(gulp.dest('./dest'));
+	gulp.src('./src/*.html')
+		.pipe(gulp.dest('./dest'))
 	done();
 });
 
-gulp.task('msg', function(done){
-	console.log('hello');
+gulp.task('img', function(done){
+	gulp.src('./src/img/*.png')
+		.pipe(imagemin())
+		.pipe(gulp.dest('./dest/img'))
 	done();
 });
 
-gulp.task('default', gulp.series('html','msg'));
+gulp.task('watch', function(done){
+	gulp.watch('./src/*.html', gulp.task('html'))
+	done();
+});
+
+gulp.task('webserver', function(done){
+	gulp.src('./dest')
+		.pipe(webserver({
+			host:'192.168.34.10',
+			port: 8000,
+			livereload: true,
+		}));
+	done();
+});
+
+gulp.task('default', gulp.series('html','img','watch','webserver'));
