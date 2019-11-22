a8afeb704f41f1b0a027bedfde825674343d227c
hpscript <43508837+hpscript@users.noreply.github.com>
2019-11-22 13:30:53 +0900

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,29 @@
 var gulp = require('gulp');
+var prettierPlugin = require('gulp-prettier-plugin');
 var imagemin = require('gulp-imagemin');
-var webserver = require('gulp-webserver');
 var sass = require('gulp-sass');
+var sassLint = require('gulp-sass-lint');
+var eslint = require('gulp-eslint');
 var plumber = require('gulp-plumber');
+var webserver = require('gulp-webserver');
+
+gulp.task('prettier', function(done){
+	gulp.src(['./src/sass/*.scss','./src/*.js'])
+		.pipe(prettierPlugin({
+			prettier:{
+				singleQuote:true
+			},
+		},{filter: true}))
+		.pipe(gulp.dest(file => file.base))
+	done();
+});
 
 gulp.task('html', function(done){
 	gulp.src('./src/*.html')
 		.pipe(gulp.dest('./dest'))
 	done();
 });
+
 
 gulp.task('img', function(done){
 	gulp.src('./src/img/*.png')
@@ -20,13 +35,27 @@ gulp.task('img', function(done){
 gulp.task('sass', function(done){
 	gulp.src('./src/sass/*.scss')
 		.pipe(plumber())
+		.pipe(sassLint())
+		.pipe(sassLint.format())
+		.pipe(sassLint.failOnError())
 		.pipe(sass({outputStyle: 'expand'}))
 		.pipe(gulp.dest('./dest/css'))
 	done();
 });
 
+gulp.task('js', function(done){
+	gulp.src(['./src/*.js','!node_modules/**'])
+		.pipe(plumber())
+		.pipe(eslint({ useEslintrc: true }))
+		.pipe(eslint.format())
+		.pipe(eslint.failAfterError())
+		.pipe(gulp.dest('./dest/js'))
+	done();
+});
+
 gulp.task('watch', function(done){
 	gulp.watch('./src/*.html', gulp.task('html'))
+	gulp.watch('./src/js/*.html', gulp.task('js'))
 	gulp.watch('./src/sass/*.scss', gulp.task('sass'))
 	done();
 });
@@ -41,4 +70,4 @@ gulp.task('webserver', function(done){
 	done();
 });
 
-gulp.task('default', gulp.series('html','img','sass','watch','webserver'));
+gulp.task('default', gulp.series('prettier','html','js','img','sass','watch','webserver'));
